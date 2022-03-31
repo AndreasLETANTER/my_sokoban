ad253c6ba0c0d153a20b1ac98ad053307d27d95e
Andréas LE TANTER <andreas.le-tanter@epitech.eu>
2022-03-31 04:07:22 -0400

    [add]: new searching method for bad files

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2022
+** MIRROR-B-DOP-200-NAN-2-1-chocolatine-andreas.le-tanter
+** File description:
+** main
+*/
+
+#include "stdlib.h"
+
+void main()
+{
+    printf("poar");
+}
